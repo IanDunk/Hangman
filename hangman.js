c07a46a80c48d07d32c4668be61cd9682b39009f
Ian <idunk007@gmail.com>
2018-01-03 16:55:04 -0800

    began creating constructors

---
+++ hangman.js
@@ -0,0 +1,11 @@
+var fruits = ["apple", "artichoke", "apricot", "asparagus", "avocado", 
+"banana", "beetroot", "blueberry", "broccoli", "brussels sprout", "butternut squash", 
+"cantaloupe", "carrot", "cherry", "clementine", "cumquat", "date", "eggplant", 
+"elderberry", "endive", "fava bean", "fennel", "fig", "garlic", "grape", 
+"grapefruit", "green bean", "guava", "honeydew", "huckleberry", "iceberg lettuce", "kiwi", 
+"leek", "lemon", "mango", "mandarin", "nectarine", "olive", "orange", "pea", 
+"peanut", "pear", "pepper", "pineapple", "plum", "pluot", "pumpkin", "quince", "raddish", 
+"raisin", "raspberry", "rhubarb", "strawberry", "sweet potato", "tomato", "turnip", 
+"watercress", "watermelon", "yam", "zucchini"];
+
+console.log(fruits[5]);  // TEST
