d9c8211f171aba2390a08e01ddb9c633ee5ae50d
Ian <idunk007@gmail.com>
2018-01-03 17:29:33 -0800

    made word talk with letter

---
+++ word.js
@@ -0,0 +1,50 @@
+var Letter = require("./letter.js");
+
+function Word(wrd) {  // format?
+    this.word = wrd;
+    this.letters = [];
+    this.wordFound = false;
+
+    // Populate letters array with letter objects
+    this.getLetters = function() {
+        for(var i = 0; i < this.word.length; i++) {
+            var newLetter = new Letter(this.word[i]);
+            this.letters.push(newLetter);
+        }
+    };
+
+    this.checkIfLetterFound = function(letterGuessed) {
+        var letterFound = false;
+
+        this.letters.forEach(function(lttr) {
+            if(lttr.letter === letterGuessed) { // make toLowerCase()
+                lttr.isShowing = true;
+                letterFound = true;
+            }
+        }) // ;?
+
+        return letterFound;
+    };
+
+    this.checkIfWordFound = function() {
+        if(this.letters.every(function(lttr) {
+            return lttr.appear === true;
+        })) {
+            this.wordFound = true;
+            return true;
+        }
+    };
+
+    this.renderWord = function() {
+        var display = "";
+        
+        this.letters.forEach(function(lttr) {
+            var currentCharacter = lttr.renderLetter();
+            display += currentCharacter;
+        })
+
+        return display;
+    };
+}
+
+module.exports = Word;
